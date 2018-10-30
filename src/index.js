9722b08af003e62421870b8056a334c569ea3735
Ladislav Navratil <ladislav.navratil@nangu.tv>
2018-10-30 23:02:21 +0100

    Init commit with transfering project to the nangutv namespace

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2017, nangu.TV, a.s. All rights reserved.
+// nangu.TV, a.s PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+//
+// @flow
+//
+
+import type { LayoutItem as _LayoutItem } from './layouts/Layout';
+import InfinityList, { ListRequest, ListTypeHorizontal, ListTypeVertical,} from './InfinityList';
+
+export type LayoutItem = _LayoutItem;
+
+export {
+    InfinityList,
+    ListRequest,
+    ListTypeHorizontal,
+    ListTypeVertical,
+};
